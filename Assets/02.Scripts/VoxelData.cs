26da94f085a79315c61bc94fdacbbaa4551cf40e
Kwonkunkun <59603575+Kwonkunkun@users.noreply.github.com>
2020-07-03 17:22:32 +0900

    Add Project of tutorial_2 of Minecraft

--- Assets/02.Scripts/VoxelData.cs
+++ Assets/02.Scripts/VoxelData.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class VoxelData
 {
+    //가로와 세로를 몇개 그릴것인지
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
+    //vertex들
     public static readonly Vector3[] voxelVerts = new Vector3[8]
     {
         new Vector3(0.0f, 0.0f, 0.0f),
@@ -16,23 +21,32 @@ public class VoxelData
         new Vector3(0.0f, 1.0f, 1.0f)
     };
 
-    public static readonly int[,] voxelTris = new int[6, 6]
-    {
-        {0, 3, 1, 1, 3, 2}, // Back Face
-		{5, 6, 4, 4, 6, 7}, // Front Face
-		{3, 7, 2, 2, 7, 6}, // Top Face
-		{1, 5, 0, 0, 5, 4}, // Bottom Face
-		{4, 7, 0, 0, 7, 3}, // Left Face
-		{1, 2, 5, 5, 2, 6} // Right Face
+    //안에 있는 면을 그리지 않기 위해서 사용
+    public static readonly Vector3[] faceChecks = new Vector3[6] {
+
+        new Vector3(0.0f, 0.0f, -1.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, -1.0f, 0.0f),
+        new Vector3(-1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f)
     };
 
-    public static readonly Vector2[] voxelUvs = new Vector2[6] 
+    public static readonly int[,] voxelTris = new int[6, 4]
+    {
+        {0, 3, 1, 2}, // Back Face
+		{5, 6, 4, 7}, // Front Face
+		{3, 7, 2, 6}, // Top Face
+		{1, 5, 0, 4}, // Bottom Face
+		{4, 7, 0, 3}, // Left Face
+		{1, 2, 5, 6} // Right Face
+    };
+
+    public static readonly Vector2[] voxelUvs = new Vector2[4] 
     {
         new Vector2 (0.0f, 0.0f),
         new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 0.0f),
-        new Vector2 (1.0f, 0.0f),
-        new Vector2 (0.0f, 1.0f),
         new Vector2 (1.0f, 1.0f)
     };
 }
